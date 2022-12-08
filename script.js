5d2b276e4ab93f1e38b80fc20c6aafbac605b5cb
ANDERSON RODRIGO <andersonhav@gmail.com>
2022-12-08 13:50:42 -0300

    ok

---
+++ script.js
@@ -0,0 +1,17 @@
+const submit = document.querySelector(".submit")
+const corpo = document.querySelector(".corpo")
+const corpo2 = document.querySelector(".corpo-2")
+const scores = document.querySelector(".score-btn")
+const scoreFinal = document.querySelector(".num")
+
+
+
+
+submit.addEventListener("click", () => {
+    corpo.style.display = "none"
+    corpo2.style.display= "block"
+    
+})
+
+
+
